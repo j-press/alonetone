e2424bcd010821f2c49cd88998c03db9680d0097
Jenya <jzueva@gmail.com>
2018-12-19 22:15:32 -0500

    Fix flickering comment unspam notification spec. It was failing because when it was trying to check comment's commentable, that was returning a nil. So ensure it's created before running the spec. Just stubbing the method didn't seem to have fixed the spec 100%

--- spec/request/admin/comments_controller_spec.rb
+++ spec/request/admin/comments_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Admin::CommentsController, type: :request do
-  fixtures :comments, :users
+  fixtures :comments, :users, :assets
 
   before do
     create_user_session(users(:sudara))
@@ -24,6 +24,9 @@ RSpec.describe Admin::CommentsController, type: :request do
   end
 
   describe "unspam individual comments" do
+    # asset id 1 is needed for comment's commentable to return true
+    # for is_deliverable?
+    let!(:asset) { assets(:valid_mp3) }
     let(:comment) { comments(:public_spam_comment_on_asset_by_user) }
 
     it "should unspam the comment" do
@@ -37,8 +40,6 @@ RSpec.describe Admin::CommentsController, type: :request do
     end
 
     it "should send notification" do
-      allow(comment).to receive(:is_deliverable?).and_return(true)
-
       expect do
         put unspam_admin_comment_path(comment.id)
       end.to change { ActionMailer::Base.deliveries.size }.by(1)
